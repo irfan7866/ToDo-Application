f4d49a98a3166b770c8f8ab6137dcf95900644e7
Irfan Gouri <2020pgicsirfan32@poornima.org>
2023-09-29 16:29:48 +0530

    fifth commit

--- Frontend/src/Views/ListingListItems.js
+++ Frontend/src/Views/ListingListItems.js
@@ -3,7 +3,11 @@ import './ListingListItems.css';
 import axios from 'axios';
 
 export default function ListingListItems({ userId }) {
+
     const [items, setItems] = useState([]);
+
+    const [editedTask, setEditedTask] = useState('');
+    const [editItemId, setEditItemId] = useState(null);
 
     useEffect(() => {
 
@@ -19,6 +23,7 @@ export default function ListingListItems({ userId }) {
         fetchListItems();
     }, [userId, items]);
 
+
     const handleDelete = async(ItemId) => {
         console.log(ItemId);
         try {
@@ -29,17 +34,44 @@ export default function ListingListItems({ userId }) {
     }
 
     
+    const handleUpdate = async(itemId, task) => {
+        setEditedTask(task);
+        setEditItemId(itemId);
+    }
+
+    const handleUpdateSave = async(itemId) => {
+        try {
+            await axios.put(`http://localhost:4000/api/list/updateListItem/${itemId}`, {
+                task: editedTask
+            });
+            setEditItemId(null);
+        } catch (error) {
+            console.error(`The error occurred is: `, error);
+        }
+    }
 
     return (
         <div className="list-item-page">
             <ul className="list">
                 {items.map((item) => (
                 <li className="list-item" key={item.id}>
-                    <span className="item-description">{item.task}</span>
-                    <div>
-                    <button className="update-button">Update</button>
-                    <button className="delete-button" onClick={() => handleDelete(item._id)}>Delete</button>
-                    </div>
+                    {editItemId === item._id ? (
+                        <>
+                            <input type='text' value={editedTask} onChange={(e) => setEditedTask(e.target.value)}/>
+                            <div>
+                                <button className="update-button" onClick={() => handleUpdateSave(item._id)}>Save</button>
+                                <button className="delete-button" onClick={() => setEditItemId(null)}>Cancel</button>
+                            </div>
+                        </>
+                    ) : (
+                        <>
+                        <span className="item-description">{item.task}</span>
+                        <div>
+                            <button className="update-button" onClick={() => handleUpdate(item._id, item.task)}>Update</button>
+                            <button className="delete-button" onClick={() => handleDelete(item._id)}>Delete</button>
+                        </div>
+                        </>
+                    )}
                 </li>
                 ))}
             </ul>
